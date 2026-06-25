c37f20130c2470606e1e3e8a9e974e349a071cb9
John Paxton <johnpaxton@gmail.com>
2026-06-25 15:39:48 -0400

    Part 1: event handling with button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import Greeter from './interactive-greeter/Greeter';
 
 function App() {
+	const [inputFirstName, setInputFirstName] = useState('Jennifer');
+
+	function sayHello() {
+		console.log('You clicked on the button.');
+		setInputFirstName('Bob');
+	}
+
 	return (
 		<main className="container">
 			<header className="row">
@@ -8,8 +16,37 @@ function App() {
 				<hr />
 			</header>
 			<section className="row">
+				{/* .col>.row*4>.col>h3{Placeholder} */}
 				<div className="col">
-					<Greeter firstName="John"></Greeter>
+					<div className="row">
+						<div className="col mb-2">
+							{/* button.btn.btn-primary */}
+							<button
+								className="btn btn-primary"
+								onClick={sayHello}
+							>
+								Change name to Bob
+							</button>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col mb-2">
+							<h3>Placeholder</h3>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col mb-2">
+							<h3>Placeholder</h3>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col">
+							<h3>Placeholder</h3>
+						</div>
+					</div>
+				</div>
+				<div className="col">
+					<Greeter firstName={inputFirstName}></Greeter>
 				</div>
 			</section>
 		</main>
