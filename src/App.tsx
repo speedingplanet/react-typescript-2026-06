12f51cf035b08bba0cd75003e2fde2cf9fabc45f
John Paxton <johnpaxton@gmail.com>
2026-06-25 16:10:21 -0400

    Example 2: updating dynamically with a button

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import { useState } from 'react';
 import Greeter from './interactive-greeter/Greeter';
 
 function App() {
+	// const [value, setter] = useState(initialState)
 	const [inputFirstName, setInputFirstName] = useState('Jennifer');
 
-	function sayHello() {
+	function handleButtonClick() {
 		console.log('You clicked on the button.');
 		setInputFirstName('Bob');
+	}
+
+	function handleButtonUpdate() {
+		let formField = document.querySelector('#button-update') as HTMLInputElement;
+
+		if (formField !== null) {
+			setInputFirstName(formField.value);
+		}
 	}
 
 	return (
@@ -23,15 +32,36 @@ function App() {
 							{/* button.btn.btn-primary */}
 							<button
 								className="btn btn-primary"
-								onClick={sayHello}
+								onClick={handleButtonClick}
 							>
 								Change name to Bob
 							</button>
 						</div>
 					</div>
+					<hr />
 					<div className="row">
 						<div className="col mb-2">
-							<h3>Placeholder</h3>
+							{/* label.form-label+input:text#button-input */}
+							<label
+								htmlFor="button-update"
+								className="form-label"
+							>
+								Enter your name:
+							</label>
+							<input
+								type="text"
+								name="buttonUpdate"
+								id="button-update"
+								className="form-control"
+							/>
+							<div className="mt-2">
+								<button
+									className="btn btn-secondary"
+									onClick={handleButtonUpdate}
+								>
+									Update
+								</button>
+							</div>
 						</div>
 					</div>
 					<div className="row">
