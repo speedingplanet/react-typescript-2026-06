d477212caf2eb27d23866d12ea52cc4d44fe9774
John Paxton <johnpaxton@gmail.com>
2026-06-28 22:53:19 -0400

    Fetch more asteroids better

--- src/data/fetch-asteroids.js
+++ src/data/fetch-asteroids.js
@@ -1,29 +1,42 @@
-import des from './first-twenty.json' with { type: 'json' };
+import des from './asteroid-designators.json' with { type: 'json' };
 import { writeJsonSync } from 'fs-extra/esm';
 
-console.log('Designations:', des);
+let count = 20;
+
+// process.argv[2] is the first command line argument
+if (process.argv[2]) {
+	count = process.argv[2];
+}
+
 let url = 'https://ssd-api.jpl.nasa.gov/sentry.api';
 
 let data = [];
 
-for (let designation of des) {
-	await fetchAsteroid(designation);
+for await (const record of fetchAsteroids()) {
+	data.push(record);
 }
 
 console.log('data: ', data);
 writeJsonSync('asteroids.json', { asteroids: data }, { spaces: '\t' });
 
+async function* fetchAsteroids() {
+	for (let designation of des.slice(0, count)) {
+		yield await fetchAsteroid(designation);
+	}
+}
+
 async function fetchAsteroid(des) {
 	let requestURL = `${url}?des=${des}`;
-	console.log(`requesting ${requestURL}`);
+	process.stdout.write(`requesting ${requestURL}...`);
 	try {
 		let response = await fetch(requestURL);
 		if (response.ok) {
 			let asteroid = await response.json();
-			data.push({
+			console.log('complete');
+			return {
 				id: des,
 				...asteroid.summary,
-			});
+			};
 		} else {
 			throw new Error(`Bad response: ${response.status}`);
 		}
